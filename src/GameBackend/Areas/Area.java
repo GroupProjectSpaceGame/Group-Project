af3b07fd06a7fbab9ae76858e2393b17a91a2550
BaToJo-napier <58687584+BaToJo-napier@users.noreply.github.com>
2019-12-09 12:26:18 +0000

    added world gen stub

--- src/GameBackend/Areas/Area.java
+++ src/GameBackend/Areas/Area.java
@@ -2,11 +2,13 @@ package GameBackend.Areas;
 
 import java.util.ArrayList;
 
+import GameBackend.Cargos.Cargo;
 import GameBackend.Vessels.Vessel;
 
 public class Area {
 
 	ArrayList<Vessel> vesselsHere = new ArrayList<>();
+	ArrayList<Cargo> cargoHere = new ArrayList<>();
 	
 	public Area() {
 		// TODO Auto-generated constructor stub
@@ -14,6 +16,10 @@ public class Area {
 
 	public ArrayList<Vessel> getVesselsList() {
 		return vesselsHere;
+	}
+
+	public ArrayList<Cargo> getCargoList() {
+		return cargoHere;
 	}
 	
 }
---
+++ src/GameWorld.java
@@ -0,0 +1,16 @@
+
+public class GameWorld {
+
+	public void generateAreas() {
+		
+	}
+	
+	public void generateShip() {
+		
+	}
+	
+	public void generateLoot() {
+		
+	}
+	
+}
